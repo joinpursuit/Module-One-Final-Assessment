d7da32c0bc6078174dc78f09e506f574463e5bfd
Mike Boyle <mike@pursuit.org>
2021-01-11 10:36:08 -0500

    Fix typo in smallest.test.js

--- __tests__/smallest.test.js
+++ __tests__/smallest.test.js
@@ -4,7 +4,7 @@ describe("smallest", () => {
   test("Returns a number", () => {
     expect(typeof smallest([5, 1, 4, 5, 3])).toBe("number")
   })
-  test("Returns the second smallest number", () => {
+  test("Returns the smallest number", () => {
     expect(smallest([5, 1, 4, 2, 5, 6])).toBe(1)
     expect(smallest([1, 10, 7, 90, 5, -4])).toBe(-4)
     expect(smallest([2, 0, 4, 90, 5, 6])).toBe(0)

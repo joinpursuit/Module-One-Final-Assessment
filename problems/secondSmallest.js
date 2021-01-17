6978d7ca3fc87363551f2d6da14316b9271f95c3
Jordan Bobadilla <jordanboba08@gmail.com>
2021-01-17 12:51:59 -0400

    secondSmallest done

--- problems/secondSmallest.js
+++ problems/secondSmallest.js
@@ -6,8 +6,22 @@
 * @returns {number} - Returns the second smallest number.
 */
 
-function secondSmallest() {
-  
+const secondSmallest = arr => {
+    let smallest = Infinity
+    let secondSmallest = Infinity
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element < smallest) {
+            smallest = element
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element > smallest && element < secondSmallest) {
+            secondSmallest = element
+        }
+    }
+    return secondSmallest
 }
 
 module.exports = secondSmallest

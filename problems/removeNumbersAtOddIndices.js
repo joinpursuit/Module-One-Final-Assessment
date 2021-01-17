fc015567ce76ec2149d79d67cc65b0e9c5423c9c
jcanizales23 <johncanizales@pursuit.org>
2021-01-17 17:39:43 -0500

    Time is running out

--- problems/removeNumbersAtOddIndices.js
+++ problems/removeNumbersAtOddIndices.js
@@ -3,7 +3,11 @@
 * @returns {number[]} - An array removing all elements initially appearing at an odd index
 */
 function removeNumbersAtOddIndices() {
-
-}
+    let arr = []
+    for(let i = 0; arr.length; i = i++)
+    if(i % 2 == 0) {
+        arr.splice(i, 0);
+    }
+};
 
 module.exports = removeNumbersAtOddIndices
--- problems/removeOddNumbers.js
+++ problems/removeOddNumbers.js
@@ -3,7 +3,11 @@
 * @returns {number[]} - The input array with all odd number removed
 */
 function removeOddNumbers() {
-
+    let arr = []
+    for(let i = 0; arr.length; i = i++)
+    if(i % 2 == 0) {
+        arr.splice(i, 0);
+    }
 }
 
 module.exports = removeOddNumbers

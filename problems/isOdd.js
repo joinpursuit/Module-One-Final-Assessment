47d1652d6ac4a9ecf3c321332ac3a25b43b13fac
jcanizales23 <johncanizales@pursuit.org>
2021-01-17 16:41:50 -0500

    Almost had the odd

--- problems/isOdd.js
+++ problems/isOdd.js
@@ -6,9 +6,15 @@
 * @returns {boolean} - True if n is odd, false if n is even, or not a number.
 
 */
+//Can't get the even to pass
+function isOdd(num) {
+   if(num % 2 === 0) {
+       return false; {
+       }
+   }else if (isNaN(false)) {
 
-function isOdd() {
-
-}
-
+   } else {
+       return false
+   }
+};
 module.exports = isOdd

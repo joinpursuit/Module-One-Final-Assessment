787997a6322bcc39bfb23cd405ccb124fd2adb9b
poonam <poonam398@gmail.com>
2021-01-17 11:44:45 -0500

    removeVowels

--- problems/removeVowels.js
+++ problems/removeVowels.js
@@ -6,8 +6,19 @@
 * @returns {string} - Returns a new string without any vowels.
 */
 
-function removeVowels() {
-
+function removeVowels(str) {
+    let vowels =["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    let newStr = ""
+    //let lowerCase = str.toLowerCase()
+    for (let i = 0; i< str.length; i++){
+        let letter = str[i]
+        if(letter !== vowels[0] && letter !== vowels[1] && letter !== vowels[2] && letter !== vowels[3] && letter !== vowels[4]
+            && letter !== vowels[5] && letter !== vowels[6] && letter !== vowels[7] && letter !== vowels[8] && letter !== vowels[9]){
+            newStr += letter
+        }
+    }
+   return newStr
 }
+console.log(removeVowels("heLLo"))
 
 module.exports = removeVowels

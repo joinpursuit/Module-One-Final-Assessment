83e8a0491fc4acb2aff353b56402ed25cb102161
poonam <poonam398@gmail.com>
2021-01-17 11:03:21 -0500

    secondSmallest

--- problems/removeNumbersAtOddIndices.js
+++ problems/removeNumbersAtOddIndices.js
@@ -2,8 +2,26 @@
 * @param {number[]} arr - The input array
 * @returns {number[]} - An array removing all elements initially appearing at an odd index
 */
-function removeNumbersAtOddIndices() {
+function removeNumbersAtOddIndices(arr) {
+    let newArr= arr.map((num,i)=>{
+        if(i%2===0){
+            return num
+        }
+    })
+    return newArr.filter((num)=> {
+        if(num !== 0){
+            return num
+        } 
+    })
+    }
 
-}
+//     return arr.filter((number, i)=>{
+//         if(i % 2 === 0 ){
+//             return number 
+//         }else if (number === 0){
+//             return false
+//         }
+//     })
+// }
 
 module.exports = removeNumbersAtOddIndices
--- problems/removeOddNumbers.js
+++ problems/removeOddNumbers.js
@@ -1,9 +1,42 @@
+const removeNumbersAtOddIndices = require("./removeNumbersAtOddIndices")
+
 /** Remove all odd numbers from an array
 * @param {number[]} arr - The input array
 * @returns {number[]} - The input array with all odd number removed
 */
-function removeOddNumbers() {
-
+function removeOddNumbers(arr) {
+    return arr.filter((number)=>{
+        if(number%2 === 0){
+            return number 
+        }
+    })
 }
+console.log(removeOddNumbers([
+    808,
+    814,
+    174,
+    98,
+    632,
+    970,
+    0,
+    554,
+    608,
+    330,
+    90,
+    976,
+    806,
+    464,
+    786,
+    590,
+    898,
+    272,
+    812,
+    52,
+    388,
+    574,
+    262,
+    698,
+    48,
+  ]))
 
 module.exports = removeOddNumbers
